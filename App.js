34dfa49649026c9ccf3de7181739524529d0b550
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-12-29 00:33:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { NavigationContainer, StackActions } from "@react-navigation/native";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase.config";
 
 import Chat from "./screens/Chat";
 import Login from "./screens/Login";
@@ -8,25 +11,67 @@ import Signup from "./screens/Signup";
 import Home from "./screens/Home";
 
 const Stack = createNativeStackNavigator();
+const AuthContext = createContext({});
+
+const AuthUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
 
 ChatStack = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown:false}}>
-      <Stack.Screen name="Login" component={Login} />
+    <Stack.Navigator screenOptions={{ headerShown: false }} defaultScreenOptions={Home}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Chat" component={Chat} />
     </Stack.Navigator>
   );
 }
 
+AuthStack = () => {
+  <Stack.Navigator screenOptions={{ headerShown: false }} defaultScreenOptions={Login}>
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Signup" component={Signup} />
+  </Stack.Navigator>
+}
+
 RootNavigator = () => {
+  const { user, setUser } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,
+      async authenticatedUser => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setLoading(false);
+      });
+
+    return () => unsubscribe();
+  }, [user]);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <ChatStack />
+      {user ? <ChatStack /> : <AuthStack />}
     </NavigationContainer>
   );
 }
 
 const home = () => {
-  return <RootNavigator />
+  return (
+    <AuthUserProvider>
+      <RootNavigator />
+    </AuthUserProvider>
+  );
 }
 
 export default home;
